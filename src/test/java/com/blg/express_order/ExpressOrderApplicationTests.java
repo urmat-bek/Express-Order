414a2c7e29fbc23545dbf4e46978fa3efa30e54e
Urmatbek Asilbekov <asilbekovurmatbek04@gmail.com>
2024-07-12 15:56:02 +0600

    first commit

---
+++ src/test/java/com/blg/express_order/ExpressOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blg.express_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
