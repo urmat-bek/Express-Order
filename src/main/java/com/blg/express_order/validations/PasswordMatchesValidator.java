865ee0b6132a77713ee749fb1f62ab304411381d
Urmatbek Asilbekov <asilbekovurmatbek04@gmail.com>
2024-07-14 15:03:19 +0600

    added validation files

---
+++ src/main/java/com/blg/express_order/validations/PasswordMatchesValidator.java
@@ -0,0 +1,19 @@
+package com.blg.express_order.validations;
+
+import com.blg.express_order.annotations.PasswordMatches;
+import com.blg.express_order.payload.request.SignUpRequest;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        SignUpRequest userSignUpRequest = (SignUpRequest) o;
+        return userSignUpRequest.getPassword().equals(userSignUpRequest.getConfirmPassword());
+    }
+}
