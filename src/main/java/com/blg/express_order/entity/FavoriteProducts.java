41083de0486ea48642f13d08c22428152ba3a606
Urmatbek Asilbekov <asilbekovurmatbek04@gmail.com>
2024-07-15 12:55:42 +0600

    added CartProducts and FavoriteProducts model

---
+++ src/main/java/com/blg/express_order/entity/FavoriteProducts.java
@@ -0,0 +1,97 @@
+package com.blg.express_order.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="favorite_products")
+public class FavoriteProducts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "rating")
+    private String rating;
+
+    @Column(name = "product_url")
+    private String productURL;
+
+    @Column(name = "image_url")
+    private String imageURL;
+
+    @Column(name = "store_name")
+    private String storeName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getProductURL() {
+        return productURL;
+    }
+
+    public void setProductURL(String productURL) {
+        this.productURL = productURL;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
